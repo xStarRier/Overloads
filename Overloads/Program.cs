63d6e922eb563ad385e162941b891a64c93c5cc9
xStarRier <stephanniequynh@gmail.com>
2024-12-17 20:11:44 +0100

    Add project files.

---
+++ Overloads/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Reflection.Emit;
+
+namespace ShowPlaces
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ShowSeperateRow();
+            var labelWidth = 20;
+            ShowFieldNameAndValue("Navn", labelWidth, "Stavern");
+            ShowFieldNameAndValue("Kommune", labelWidth, "Larvik");
+            ShowFieldNameAndValue("Fylke", labelWidth, "Vestfold");
+            ShowSeperateRow();
+
+
+
+        }
+
+        static void ShowFieldNameAndValue(string label, int labelWidth, string fieldValue)
+        {
+            labelWidth -= label.Length;
+            Console.WriteLine("  " + label + ":" + string.Empty.PadLeft(labelWidth, ' ') + fieldValue);
+
+
+        }
+
+        static void ShowSeperateRow()
+        {
+            Console.WriteLine("------------------");
+          
+
+        }
+
+
+
+    }
+
+
+}
